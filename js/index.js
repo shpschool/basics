7db7f242855dc876f5cab9b2813fe579a06083a5
Galya <54934835+iamgo100@users.noreply.github.com>
2021-08-20 19:02:30 +0300

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,24 @@
+const body = document.body;
+
+body.innerHTML = `
+<div id="cont">
+<label>Введи полученную фразу:</label>
+<input id="answer-input">
+<button id="btn">Отправить ответ</button>
+<p></p>
+<p id="answer"></p>
+</div>
+`
+
+const input = document.getElementById('answer-input');
+const btn = document.getElementById('btn');
+const answer = document.getElementById('answer');
+
+btn.addEventListener('click', () => {
+    let answ = input.value;
+    if (answ.toLowerCase === 'красная звезда укажет путь к времени') {
+        answer.textContent = 'Ответ верный. Условие след. задачи - вот.';
+    } else {
+        answer.textContent = 'Ответ неверный. Проверь свою запись и повтори попытку.';
+    }
+});

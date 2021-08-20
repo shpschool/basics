c721be5c60a4061b4712741ffeee4d3701036c8f
Galya <54934835+iamgo100@users.noreply.github.com>
2021-08-20 19:14:54 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -16,7 +16,7 @@ const answer = document.getElementById('answer');
 
 btn.addEventListener('click', () => {
     let answ = input.value;
-    if (answ.toLowerCase === 'красная звезда укажет путь к времени') {
+    if (answ.toLowerCase() === 'красная звезда укажет путь к времени') {
         answer.textContent = 'Ответ верный. Условие след. задачи - вот.';
     } else {
         answer.textContent = 'Ответ неверный. Проверь свою запись и повтори попытку.';

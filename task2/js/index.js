f50c73924c9b2577b9895c8d0c0b31f3999ac842
Galya <54934835+iamgo100@users.noreply.github.com>
2021-10-14 18:33:18 +0300

    Delete index.js

--- task2/js/index.js
+++
@@ -1,7 +0,0 @@
-const body = document.body;
-
-body.innerHTML = `
-<div id="cont">
-<iframe src="https://scratch.mit.edu/projects/534588677/embed" allowtransparency="true" width="600" height="450" frameborder="0" scrolling="no" allowfullscreen></iframe>
-</div>
-`

b1723557a4b83b98ba635af9f1beee58b66c486c
iamgo100 <gufkz.tkrbyf@yandex.ru>
2021-10-29 02:42:42 +0300

    add task3

--- task3/js/task3.js
+++ task3/js/task3.js
@@ -2,9 +2,12 @@ const prompt = document.getElementById('prompt');
 const btn = document.getElementById('btn');
 const code = document.getElementById('code');
 
+const wrongAnsw = () => {
+    prompt.innerHTML = `<p>Код неверный. Проверь свою запись и повтори попытку.</p>`
+};
+
 const prompt1 = (ctg, src) => {
-    prompt.innerHTML = `
-        <h3>Подсказка категории "${ctg}"</h3>
+    prompt.innerHTML = `<h3>Подсказка категории "${ctg}"</h3>
         <p>Посмотри на картинку и сделай свои выводы.</p>
         <img id="prompt-img" src="captures/${src}">`;
 };
@@ -13,9 +16,12 @@ btn.addEventListener('click', () => {
     switch (code.value) {
         case '212361':
             prompt1('КТО', '212361.jpg');
-            break
+            break;
         case '690108':
             prompt1('КТО', '690108.png');
-            break
+            break;
+        default:
+            wrongAnsw();
+            break;
     }
 });

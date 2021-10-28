f2697e2071a102d05d43474ee6a77ef32c365ea6
iamgo100 <gufkz.tkrbyf@yandex.ru>
2021-10-29 02:32:09 +0300

    add task3

---
+++ task3/js/task3.js
@@ -0,0 +1,21 @@
+const prompt = document.getElementById('prompt');
+const btn = document.getElementById('btn');
+const code = document.getElementById('code');
+
+const prompt1 = (ctg, src) => {
+    prompt.innerHTML = `
+        <h3>Подсказка категории "${ctg}"</h3>
+        <p>Посмотри на картинку и сделай свои выводы.</p>
+        <img id="prompt-img" src="captures/${src}">`;
+};
+
+btn.addEventListener('click', () => {
+    switch (code.value) {
+        case '212361':
+            prompt1('КТО', '212361.jpg');
+            break
+        case '690108':
+            prompt1('КТО', '690108.png');
+            break
+    }
+});

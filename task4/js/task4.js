12bc8e868263dbc6ceeaf1ff0d105c2a604bd7d8
iamgo100 <gufkz.tkrbyf@yandex.ru>
2021-12-01 15:11:46 +0300

    update task4

--- task4/js/task4.js
+++ task4/js/task4.js
@@ -1,9 +1,9 @@
-const scene = document.getElementById('scene');
-const progress1 = document.getElementById('strenght');
-const progress2 = document.getElementById('power');
-const powerCont = document.getElementById('using-power');
-const btn = document.getElementById('btn');
-const descr = document.getElementById('description');
+const scene = document.querySelectorAll('#scene');
+const progress1 = document.querySelectorAll('#strenght');
+const progress2 = document.querySelectorAll('#power');
+const powerCont = document.querySelectorAll('#using-power');
+const btn = document.querySelectorAll('#btn');
+const descr = document.querySelectorAll('#description');
 const herous = document.querySelectorAll('.hero');
 const codes = document.querySelectorAll('.text')
 
@@ -29,10 +29,12 @@ const showPower = async (heroInd, code) => {
     // let res = await fetch('db/codes.json');
     // res = await res.json();
     // console.log(res);
-    description = '<p>Введи код для выбранного героя, чтобы увидеть описание силы</p>';
-    descr.innerHTML = description;
-    progress2.value = 30;
-    changeProgressStyle(progress2);
+    description = 'Введи код для выбранного героя, чтобы увидеть описание силы';
+    descr.forEach((el) => el.textContent = description);
+    progress2.forEach((el) => {
+        el.value = 30;
+        changeProgressStyle(el);
+    });
 };
 
 const hideCodes = () => {
@@ -43,8 +45,9 @@ const hideCodes = () => {
 }
 
 hideCodes();
-scene.innerHTML = '<img src="captures/hero0.png" id="hero0">';
-descr.innerHTML = '<p>Введи код для выбранного героя, чтобы увидеть описание силы</p>';
+scene.forEach((el) => el.innerHTML = '<img src="captures/hero0.png" id="hero0">');
+description = 'Введи код для выбранного героя, чтобы увидеть описание силы';
+descr.forEach((el) => el.textContent = description);
 
 herous.forEach((el, ind) => {
     const hero = el.children[1];
@@ -58,8 +61,12 @@ herous.forEach((el, ind) => {
     });
 });
 
-btn.addEventListener('click', () => {
-    progress1.value -= progress2.value;
-    changeProgressStyle(progress1);
-    hideCodes();
+btn.forEach((butn) => {
+    butn.addEventListener('click', () => {
+        progress1.forEach((el) => {
+            el.value -= progress2[0].value;
+            changeProgressStyle(el);
+        });
+        hideCodes();
+    });
 });
